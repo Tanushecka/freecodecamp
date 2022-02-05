afbe1c76fa8c598f65553729d59a343f2f32110a
Tanushecka <tanushecka@tut.by>
2022-02-05 17:59:13 +0300

    Declare a Read-Only Variable with the const Keyword

---
+++ 10.js
@@ -0,0 +1,5 @@
+let fcc = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+const FCC = "freeCodeCamp";
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
